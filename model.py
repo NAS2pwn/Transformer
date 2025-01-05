cc748e336a028c9f10d9569609caff9d96b9e414
NAS2pwn <nassim.bennouar.mail@gmail.com>
2025-01-06 00:35:52 +0100

    encoder finished

--- model.py
+++ model.py
@@ -114,3 +114,30 @@ class ResidualConnection(nn.Module):
     def forward(self, x, sublayer):
         return x + self.dropout(sublayer(self.norm(x)))
         # On peut appliquer la normalisation avant ou après la sublayer, dans le papier c'est après
+
+class EncoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, # Query
+                                                                                x, # Key
+                                                                                x, #Value
+                                                                                src_mask)) # Mask
+        # Encoder : self-attention -> feed-forward ; Decoder : self-attention -> feed-forward -> cross-attention
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+    
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, src_mask):
+        for layer in self.layers:
+            x = layer(x, src_mask)
+        return self.norm(x)
