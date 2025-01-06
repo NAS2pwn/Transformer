91319a0f3fbd7361db7178ca3fb718915b56d7fd
NAS2pwn <nassim.bennouar.mail@gmail.com>
2025-01-06 22:57:32 +0100

    projection layer

--- model.py
+++ model.py
@@ -168,3 +168,13 @@ class Decoder(nn.Module):
         for layer in self.layers:
             x = layer(x, enc_out, src_mask, target_mask)
         return self.norm(x)
+
+class ProjectionLayer(nn.Module):
+
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x):
+        return torch.log_softmax(self.proj(x), dim=-1)
+    
