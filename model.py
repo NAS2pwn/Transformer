6c5c8036dafe6759ee1ce30e28e8f470f86fc147
NAS2pwn <nassim.bennouar.mail@gmail.com>
2025-01-06 23:00:04 +0100

    transformer

--- model.py
+++ model.py
@@ -178,3 +178,32 @@ class ProjectionLayer(nn.Module):
     def forward(self, x):
         return torch.log_softmax(self.proj(x), dim=-1)
     
+class Transformer(nn.Module):
+    def __init__(self, encoder: Encoder,
+                 decoder: Decoder,
+                 src_embed: InputEmbeddings,
+                 target_embed: InputEmbeddings,
+                 src_position : PositionalEncoding,
+                 target_position : PositionalEncoding,
+                 projection_layer: ProjectionLayer):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.target_embed = target_embed
+        self.src_position = src_position
+        self.target_position = target_position
+        self.projection_layer = projection_layer
+
+    def encode(self, src, src_mask):
+        src = self.src_embed(src)
+        src = self.src_position(src)
+        return self.encoder(src, src_mask)
+    
+    def decode(self, encoder_out, src_mask, target, target_mask):
+        target = self.target_embed(target)
+        target = self.target_position(target)
+        return self.decoder(target, encoder_out, src_mask, target_mask)
+    
+    def project(self, x):
+        return self.projection_layer(x)
