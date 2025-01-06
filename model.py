189aed4e2801880ac8120f491f353d7f631275d8
NAS2pwn <nassim.bennouar.mail@gmail.com>
2025-01-07 00:21:03 +0100

    transformer

--- model.py
+++ model.py
@@ -207,3 +207,47 @@ class Transformer(nn.Module):
     
     def project(self, x):
         return self.projection_layer(x)
+
+def build_transformer(src_vocab_size: int,
+                      target_vocab_size: int,
+                      src_seq_len: int,
+                      target_seq_len: int,
+                      d_model: int = 512,
+                      N: int = 6,
+                      h: int = 8,
+                      dropout: float = 0.1,
+                      d_ff: int = 2048):
+    src_embed = InputEmbeddings(d_model, src_vocab_size)
+    target_embed = InputEmbeddings(d_model, target_vocab_size)
+
+    src_position = PositionalEncoding(d_model, src_seq_len, dropout)
+    target_position = PositionalEncoding(d_model, target_seq_len, dropout)
+
+    encoder_blocks = []
+    for _ in range(N):
+        encoder_self_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        encoder_block = EncoderBlock(encoder_self_attention_block, feed_forward_block, dropout)
+        encoder_blocks.append(encoder_block)
+    
+    decoder_blocks = []
+    for _ in range(N):
+        decoder_self_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        decoder_cross_attention_block = MultiHeadAttentionBlock(d_model, h, dropout)
+        feed_forward_block = FeedForwardBlock(d_model, d_ff, dropout)
+        decoder_block = DecoderBlock(decoder_self_attention_block, decoder_cross_attention_block, feed_forward_block, dropout)
+        decoder_blocks.append(decoder_block)
+
+    encoder = Encoder(nn.ModuleList(encoder_blocks))
+    decoder = Decoder(nn.ModuleList(decoder_blocks))
+
+    projection_layer = ProjectionLayer(d_model, target_vocab_size)
+
+    transformer = Transformer(encoder, decoder, src_embed, target_embed, src_position, target_position, projection_layer)
+
+    # Initialize the weights
+    for p in transformer.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p) # Je comprends pas exactement pourquoi c'est utile
+
+    return transformer  
