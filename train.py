f2d3e7b0ded4c8f4a582f62c3500b54b1e981815
NAS2pwn <nassim.bennouar.mail@gmail.com>
2025-01-09 00:30:20 +0100

    tokenizer

---
+++ train.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+from torch.utils.data import random_split
+from pathlib import Path
+
+from dataset import BilingualDataset
+
+def get_all_sentences(dataset, lang):
+    for item in dataset:
+        yield item["translation"][lang]
+
+def get_or_build_tokenizer(config, dataset, lang):
+    tokenizer_path = Path(config["tokenizer_path"].fomat(lang))
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=["[UNK]"], min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(dataset, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer
+
+def load_dataset(config):
+    ds_raw = load_dataset("opus_books", f"{config['lang_src']}-{config['lang_target']}", split="train")
+    
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config["lang_src"])
+    tokenizer_target = get_or_build_tokenizer(config, ds_raw, config["lang_target"])
+
+    # Training 90%, validation 10%
+    train_ds_size = int(len(ds_raw) * 0.9)
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_target, config["lang_src"], config["lang_target"], config["seq_len"])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_target, config["lang_src"], config["lang_target"], config["seq_len"])
+
+    return train_ds, val_ds
